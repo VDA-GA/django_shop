ffea18c7f920bd603c3cada8a1df389645a701f2
Dmitry Vasilenko <vasilenkoda91@gmail.com>
2024-02-18 19:13:18 +0300

    HW-3 Tasks 1-3

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100, verbose_name='Название категории')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.category_name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=250, verbose_name='Наименование')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+    picture = models.ImageField(upload_to='catalog/', **NULLABLE, verbose_name='Превью')
+    #category = models.CharField(max_length=100, verbose_name='Категория')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.DecimalField(decimal_places=2, max_digits=12, verbose_name='Цена')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.title} - {self.price}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
