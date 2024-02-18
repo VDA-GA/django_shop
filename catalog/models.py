97af7149907eee89b010d3867fb971a8b4592137
Dmitry Vasilenko <vasilenkoda91@gmail.com>
2024-02-18 19:21:46 +0300

    HW-3 Task 4 delete manufactured_at

--- catalog/models.py
+++ catalog/models.py
@@ -19,12 +19,10 @@ class Product(models.Model):
     title = models.CharField(max_length=250, verbose_name='Наименование')
     description = models.TextField(**NULLABLE, verbose_name='Описание')
     picture = models.ImageField(upload_to='catalog/', **NULLABLE, verbose_name='Превью')
-    #category = models.CharField(max_length=100, verbose_name='Категория')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     price = models.DecimalField(decimal_places=2, max_digits=12, verbose_name='Цена')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
-    manufactured_at = models.DateTimeField(**NULLABLE, verbose_name='Дата производства продукта')
 
     def __str__(self):
         return f'{self.title} - {self.price}'
