40fc1490d7e9ee77ebee6b9ebd55e80cf4a804d8
Dmitry Vasilenko <vasilenkoda91@gmail.com>
2024-02-25 14:41:31 +0300

    HW-4 Task-2

--- catalog/views.py
+++ catalog/views.py
@@ -4,7 +4,8 @@ from catalog.models import Product
 
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    context = {'object_list': Product.objects.all}
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts(request):
