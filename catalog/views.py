ab6c7791af72f8514f14bd1ece2a29f694f66951
Dmitry Vasilenko <vasilenkoda91@gmail.com>
2024-02-25 14:00:37 +0300

    HW-4 Task-1

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def home(request):
     return render(request, 'catalog/home.html')
@@ -12,3 +14,8 @@ def contacts(request):
         message = request.POST.get('message')
         print(f'{name} - {phone}: {message}')
     return render(request, 'catalog/contacts.html')
+
+
+def product(request, pk):
+    context = {'object': Product.objects.get(pk=pk)}
+    return render(request, 'catalog/product_page.html', context)
